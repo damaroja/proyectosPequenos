bd210b2d3354f8240e21165e90bb6cb61c94a64e
damaroja <ildefonsoarrecife3@hotmail.com>
2024-01-24 10:37:58 +0000

    Update index.js

--- RollDice/index.js
+++ RollDice/index.js
@@ -1,59 +1,76 @@
-const rollBtn = document.querySelector("#roll-btn");
-const dice = document.querySelector("#dice");
-const history = document.querySelector("#roll-history");
-const media = document.querySelector("#media");
-const distribucion = document.querySelector("#distribucion");
-
-let historyList = new Array();
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  rollBtn.addEventListener("click", () => {
-    getNumber();
-  });
-});
+/* Funciones de calculo de tiradas */
 
-function getNumber() {
-  dice.classList.add("roll-animation");
-  setTimeout(() => {
-    dice.classList.remove("roll-animation");
-  }, 1000);
-  const randomNumber = Math.floor(Math.random() * 6) + 1;
-  const face = getDiceFace(randomNumber);
-  dice.innerHTML = face
-  historyList.push(face);
-  renderHistory();
+const dadoAleatorio = () => {
+    return Math.floor(Math.random() * 6) + 1;
 }
 
-function getDiceFace(n) {
-  switch (n) {
-    case 1:
-      return "&#9856;";
-    case 2:
-      return "&#9857;";
-    case 3:
-      return "&#9858;";
-    case 4:
-      return "&#9859;";
-    case 5:
-      return "&#9860;";
-    default:
-      return "&#9861;";
-  }
+const dado = {
+    '1': '⚀',
+    '2': '⚁',
+    '3': '⚂',
+    '4': '⚃',
+    '5': '⚄',
+    '6': '⚅'
 }
 
-function renderHistory() {
-    while (history.firstChild) {
-        history.removeChild(history.firstChild);
+let results = new Array();
+
+const btn = document.querySelector('#btn');
+const dice = document.querySelector('#dice');
+const data = document.querySelector('#data');
+const resetBtn = document.querySelector('#resetBtn');
+const tiradas = document.querySelector('#tiradas');
+const estadisticasBtn = document.querySelector('#estadisticas');
+
+document.addEventListener('DOMContentLoaded', () => {
+    btn.addEventListener('click', () => {
+        dice.textContent = dado[dadoAleatorio()];
+        results.push(dice.textContent);
+        mostrarResultados();
+    });
+    resetBtn.addEventListener('click', () => {
+        results = [];
+        mostrarResultados();
+    });
+}); 
+
+
+/* Funciones de mostrar resultados */
+const mostrarResultados = () => {
+    if(results.length > 0) {
+        data.innerHTML = '';
+        results.forEach((result, index) => {
+            data.innerHTML += `<li><span>Roll ${index}:</span>  <span>${result}</span></li>`;
+        });
+        tiradas.textContent = results.length;
+        estadisticasBtn.textContent = JSON.stringify(getStadistics());
+        return
     }
-    let n = 1
-    historyList.forEach((face) => {
-        const li = document.createElement("li");
-        li.innerHTML = `Roll ${n}: <span>${face}</span>`;
-        history.appendChild(li);
-        n++
-    })
+    data.innerHTML = '';
+    return
 }
+
+const getStadistics = () => {
+    let stadistics = {
+        '⚀': 0,
+        '⚁': 0,
+        '⚂': 0,
+        '⚃': 0,
+        '⚄': 0,
+        '⚅': 0
+    }
+    results.forEach(result => {
+        stadistics[result]++;
+    });
+    return stadistics;
+}
+
+
+
+
+
 
 
 
